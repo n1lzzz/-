fc80af82cc15f2ade4a6c577cc31e5f6bb258aac
n1lzzz <73069621+n1lzzz@users.noreply.github.com>
2020-10-21 16:39:34 +0200

    Add files via upload

---
+++ Cosmetics.java
@@ -0,0 +1,25 @@
+package packagename.gui;
+
+import java.io.File;
+import packagename.FileManager;
+
+public class Cosmetics {
+	
+	public static boolean statusCosmetic1;
+	public static boolean statusCosmetic1;
+	public static boolean statusCosmetic1;
+	
+	public Cosmetics(boolean statusCosmetic1, boolean statusCosmetic2, boolean statusCosmetic3) {
+		dontknowhowtonamethis2(statusCosmetic1, statusCosmetic3, statusCosmetic3);
+	}
+
+	public static BooleansCosmetics dontknowhowtonamethis(boolean cosmeticWitherStatus, boolean cosmeticCreeperStatus, boolean cosmeticGhostWingsStatus) {
+		return new Cosmetics(statusCosmetic1, statusCosmetic2, statusCosmetic3);
+	}
+	
+	public void dontknowhowtonamethis2(boolean statusCosmetic1, boolean statusCosmetic2, boolean statusCosmetic3) {
+		this.statusCosmetic1 = statusCosmetic1;
+		this.statusCosmetic2 = statusCosmetic2;
+		this.statusCosmetic3 = statusCosmetic3;
+	}
+}
